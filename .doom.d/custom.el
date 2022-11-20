a51702e3519dc969d9b952e3764a313b021c90f3
Yifan Song <Eric_YF_Song@outlook.com>
2022-11-21 00:19:02 +0100

    Update doom emacs config

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,46 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#0D0E16" "#D83441" "#79D836" "#D8B941" "#3679D8" "#8041D8" "#36D8BD" "#CEDBE5"])
  '(custom-safe-themes
-   '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" default))
- '(exwm-floating-border-color "#0F1019")
- '(fci-rule-color "#767676")
- '(highlight-tail-colors
-   ((("#172219" "color-113" "green")
-     . 0)
-    (("#112226" "color-79" "cyan")
-     . 20)))
- '(jdee-db-active-breakpoint-face-colors (cons "#0F1019" "#D85F00"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#0F1019" "#79D836"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#0F1019" "#767676"))
- '(objed-cursor-color "#D83441")
- '(pdf-view-midnight-colors (cons "#CEDBE5" "#0D0E16"))
- '(rustic-ansi-faces
-   ["#0D0E16" "#D83441" "#79D836" "#D8B941" "#3679D8" "#8041D8" "#36D8BD" "#CEDBE5"])
- '(vc-annotate-background "#0D0E16")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#79D836")
-    (cons 40 "#98cd39")
-    (cons 60 "#b8c33d")
-    (cons 80 "#D8B941")
-    (cons 100 "#d89b2b")
-    (cons 120 "#d87d15")
-    (cons 140 "#D85F00")
-    (cons 160 "#ba5548")
-    (cons 180 "#9d4b90")
-    (cons 200 "#8041D8")
-    (cons 220 "#9d3ca5")
-    (cons 240 "#ba3873")
-    (cons 260 "#D83441")
-    (cons 280 "#bf444e")
-    (cons 300 "#a7555b")
-    (cons 320 "#8e6568")
-    (cons 340 "#767676")
-    (cons 360 "#767676")))
- '(vc-annotate-very-old-color nil))
+   '("246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" default))
+ '(org-agenda-files
+   '("~/org/ai/01-probability.org" "/home/yifan/org/ai/04-QuantificationRewardHacking.org" "/home/yifan/org/ai/03-BasicsOfML.org" "/home/yifan/org/0-Index.org" "/home/yifan/org/6-Tuple.org" "/home/yifan/org/7-Functions.org" "/home/yifan/org/8-Lexically-Scoped.org" "/home/yifan/org/tryorg.org"))
+ '(package-selected-packages '(wakatime-mode))
+ '(wakatime-cli-path "/home/yifan/.wakatime/wakatime-cli"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- .doom.d/init.el
+++ .doom.d/init.el
@@ -31,7 +31,8 @@
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       ;;(emoji +unicode)  ; 🙂
+       (emoji +unicode   ; 🙂
+        +github)
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
@@ -43,19 +44,20 @@
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
-       (treemacs +lsp)          ; a project drawer, like neotree but cooler
-       ;;unicode           ; extended unicode support for various languages
+       (treemacs           ; a project drawer, like neotree but cooler
+        +lsp)
+       unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
-       ;;zen               ; distraction-free coding or writing
+       zen               ; distraction-free coding or writing
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       (format +onsave)  ; automated prettiness
+       (format )  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
@@ -80,11 +82,12 @@
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       (spell +flyspell) ; tasing you for misspelling mispelling
+       ;;(spell +flyspell) ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
+       ;;biblio            ; Writes a PhD for you (citation needed)
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
        ;;docker
@@ -93,17 +96,18 @@
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       (lsp +peek)               ; M-x vscode
+       (lsp +eglot +peek)              ; M-x vscode
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
-       ;;pass              ; password manager for nerds
+       pass              ; password manager for nerds
        pdf               ; pdf enhancements
-       ;;prodigy           ; FIXME managing external services & code builders
-       ;;rgb               ; creating color strings
-       ;;taskrunner        ; taskrunner for all your projects
-       ;;terraform         ; infrastructure as code
-       ;;tmux              ; an API for interacting with tmux
-       ;;upload            ; map local to remote projects via ssh/ftp
+       prodigy           ; FIXME managing external services & code builders
+       rgb               ; creating color strings
+       taskrunner        ; taskrunner for all your projects
+       terraform         ; infrastructure as code
+       tmux              ; an API for interacting with tmux
+       upload            ; map local to remote projects via ssh/ftp
+       tree-sitter
 
        :os
        (:if IS-MAC macos)  ; improve compatibility with macOS
@@ -112,12 +116,12 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
-       (cc +lsp)                ; C > C++ == 1
+       ;;cc                ; C > C++ == 1
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
-       (csharp +lsp +unity +dotnet)       ; unity, .NET, and mono shenanigans
+       ;;csharp            ; unity, .NET, and mono shenanigans
        ;;data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;dhall
@@ -128,27 +132,28 @@
        ;;ess               ; emacs speaks statistics
        ;;factor
        ;;faust             ; dsp, but you get to keep your soul
-       (fsharp +lsp)            ; ML stands for Microsoft's Language
+       ;;fortran           ; in FORTRAN, GOD is REAL (unless declared INTEGER)
+       ;;fsharp            ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
        ;;(go +lsp)         ; the hipster dialect
-       ;;(haskell +lsp)    ; a language that's lazier than I am
+       (haskell +lsp)    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
-       (json +lsp)              ; At least it ain't XML
+       ;;json              ; At least it ain't XML
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       (latex +cdlatex +lsp)             ; writing papers in Emacs has never been so fun
-       ;;lean              ; for folks with too much to prove
+       ;;latex             ; writing papers in Emacs has never been so fun
+       lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        ;;lua               ; one-based indices? one-based indices
-       (markdown +grip)         ; writing docs for people to ignore
+       markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
-       ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (ocaml +lsp +tree-sitter)             ; an objective camel
+       (org +pandoc +present +roam2 +pretty +jupyter)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
@@ -159,8 +164,8 @@
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
-       ;;scala             ; java, but good
+       (rust +lsp)              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (scala +lsp)             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
@@ -185,5 +190,5 @@
        ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
-       ;;literate
+       literate
        (default +bindings +smartparens))
