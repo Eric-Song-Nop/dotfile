25be64e3e8bf248b5b27d688af1f48f8ffee181d
Yifan Song <Eric_YF_Song@outlook.com>
2022-11-21 01:10:04 +0100

    Add custom todo keywords

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -11,3 +11,19 @@
 ;;   (call-process "import" nil nil nil filename)
 ;;   (insert (concat "[[" filename "]]"))
 ;;   (org-display-inline-images))
+
+(after! org
+  (setq
+   org-bullets-bullet-list '("⁖")
+   org-ellipsis " ... "
+   org-todo-keywords '((sequence "TODO(t)" "PROGRESS(p)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)"))
+   org-todo-keyword-faces
+   '(("TODO" :foreground "#7c7c75" :weight normal :underline t)
+     ("WAITING" :foreground "#9f7efe" :weight normal :underline t)
+     ("PROGRESS" :foreground "#0098dd" :weight normal :underline t)
+     ("DONE" :foreground "#50a14f" :weight normal :underline t)
+     ("CANCELLED" :foreground "#ff6480" :weight normal :underline t))
+   org-priority-faces '((65 :foreground "#e45649")
+                        (66 :foreground "#da8548")
+                        (67 :foreground "#0098dd"))
+   ))
