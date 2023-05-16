7fe9fa3b248a4ec8a2de4815e0d5d15718e015c5
Yifan Song <Eric_YF_Song@outlook.com>
2023-05-16 11:01:43 +0200

    Update doom config

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" default))
  '(org-agenda-files
-   '("~/org/ai/01-probability.org" "/home/yifan/org/ai/04-QuantificationRewardHacking.org" "/home/yifan/org/ai/03-BasicsOfML.org" "/home/yifan/org/0-Index.org" "/home/yifan/org/6-Tuple.org" "/home/yifan/org/7-Functions.org" "/home/yifan/org/8-Lexically-Scoped.org" "/home/yifan/org/tryorg.org"))
+   '("~/org/TODO.org" "/home/yifan/org/ai/01-probability.org" "/home/yifan/org/ai/04-QuantificationRewardHacking.org" "/home/yifan/org/ai/03-BasicsOfML.org" "/home/yifan/org/0-Index.org" "/home/yifan/org/6-Tuple.org" "/home/yifan/org/7-Functions.org" "/home/yifan/org/8-Lexically-Scoped.org" "/home/yifan/org/tryorg.org"))
  '(package-selected-packages '(wakatime-mode))
  '(wakatime-cli-path "/home/yifan/.wakatime/wakatime-cli"))
 (custom-set-faces
