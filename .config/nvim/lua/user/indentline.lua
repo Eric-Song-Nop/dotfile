1749364912e954bf75d95a8cf6e095ab494a771a
Yifan Song <Eric_YF_Song@outlook.com>
2023-08-01 14:43:35 +0800

    Merge some plugins into one file

--- .config/nvim/lua/user/indentline.lua
+++ .config/nvim/lua/user/indentline.lua
@@ -1,20 +1,38 @@
 local M = {
-  "lukas-reineke/indent-blankline.nvim",
-  event = "BufReadPre",
-}
-
-M.opts = {
-  char = "▏",
-  show_trailing_blankline_indent = false,
-  show_first_indent_level = true,
-  use_treesitter = true,
-  show_current_context = true,
-  buftype_exclude = { "terminal", "nofile" },
-  filetype_exclude = {
-    "help",
-    "packer",
-    "NvimTree",
-  },
+    -- {
+    --     "lukas-reineke/indent-blankline.nvim",
+    --     event = "BufReadPre",
+    --     opts = {
+    --         char = "▏",
+    --         show_trailing_blankline_indent = false,
+    --         show_first_indent_level = true,
+    --         use_treesitter = true,
+    --         show_current_context = true,
+    --         buftype_exclude = { "terminal", "nofile" },
+    --         filetype_exclude = {
+    --             "help",
+    --             "packer",
+    --             "NvimTree",
+    --         },
+    --     },
+    -- },
+    {
+        "shellRaining/hlchunk.nvim",
+        event = { "UIEnter" },
+        config = function()
+            require("hlchunk").setup {
+                chunk = {
+                    notify = false,
+                    style = {
+                        { fg = "#806d9c" },
+                    },
+                },
+                blank = {
+                    enable = false,
+                },
+            }
+        end,
+    },
 }
 
 return M
--- .config/nvim/lua/user/symbolsoutline.lua
+++ .config/nvim/lua/user/symbolsoutline.lua
@@ -1,9 +1,30 @@
 local M = {
-    "simrat39/symbols-outline.nvim",
-    cmd = "SymbolsOutline",
-    config = function()
-        require("symbols-outline").setup()
-    end,
+    {
+        "simrat39/symbols-outline.nvim",
+        cmd = "SymbolsOutline",
+        config = function()
+            require("symbols-outline").setup()
+        end,
+    },
+    {
+        "stevearc/aerial.nvim",
+        -- dev = true,
+        opts = {},
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons",
+        },
+        cmd = "AerialToggle",
+        config = function()
+            require("aerial").setup {
+                --     -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+                --     on_attach = function(bufnr)
+                --         -- Jump forwards/backwards with '{' and '}'
+                --         vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+                --         vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+                --     end,
+            }
+        end,
+    },
 }
-
 return M
